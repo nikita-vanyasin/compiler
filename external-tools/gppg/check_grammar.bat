a7868902078508aa80e7f33227f08705d95e4adc
nikita_vanyasin <nikita.vanyasin@gmail.com>
2014-01-02 17:22:42 +0300

    Исправлены конфликты типа свертка/свертка

--- external-tools/gppg/check_grammar.bat
+++ external-tools/gppg/check_grammar.bat
@@ -2,7 +2,7 @@
 ECHO checkout last Parser.y
 COPY ..\..\docs\grammar\Parser.y Parser.y
 ECHO runing gppg...
-CALL gppg.exe /listing Parser.y  > t_generated.cs
+CALL gppg.exe /listing /verbose /conflicts /report Parser.y  > t_generated.cs
 CALL DEL t_generated.cs
 ECHO scanning listing for warnings...
 CALL findstr /i /m /c:"Warning:" Parser.lst | find /c "Parser.lst"
