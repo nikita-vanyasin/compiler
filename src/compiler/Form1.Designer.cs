4d6758054cf7cb1da637b82bb450ce9998e0a7e3
nikita_vanyasin <nikita.vanyasin@gmail.com>
2014-01-09 00:25:53 +0300

    Небольшие поправки

--- src/compiler/Form1.Designer.cs
+++ src/compiler/Form1.Designer.cs
@@ -28,109 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.buildToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
-			this.logListBox = new System.Windows.Forms.ListBox();
-			this.SourceBox = new System.Windows.Forms.TextBox();
-			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-			this.menuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.logListBox = new System.Windows.Forms.ListBox();
+            this.SourceBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.buildToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-			this.menuStrip1.Size = new System.Drawing.Size(802, 24);
-			this.menuStrip1.TabIndex = 0;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// fileToolStripMenuItem
-			// 
-			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1069, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem1,
             this.saveToolStripMenuItem});
-			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-			this.fileToolStripMenuItem.Text = "File";
-			// 
-			// openToolStripMenuItem1
-			// 
-			this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
-			this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-			this.openToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-			this.openToolStripMenuItem1.Text = "Open";
-			this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-			// 
-			// saveToolStripMenuItem
-			// 
-			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-			this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-			this.saveToolStripMenuItem.Text = "Save as";
-			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
-			// 
-			// buildToolStripMenuItem
-			// 
-			this.buildToolStripMenuItem.Name = "buildToolStripMenuItem";
-			this.buildToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
-			this.buildToolStripMenuItem.Text = "Build";
-			this.buildToolStripMenuItem.Click += new System.EventHandler(this.buildToolStripMenuItem_Click);
-			// 
-			// openFileDlg
-			// 
-			this.openFileDlg.DefaultExt = "ps";
-			this.openFileDlg.Filter = "PySharp Files (*.ps) |*.ps";
-			// 
-			// logListBox
-			// 
-			this.logListBox.FormattingEnabled = true;
-			this.logListBox.Location = new System.Drawing.Point(12, 486);
-			this.logListBox.Margin = new System.Windows.Forms.Padding(2);
-			this.logListBox.Name = "logListBox";
-			this.logListBox.Size = new System.Drawing.Size(778, 56);
-			this.logListBox.TabIndex = 2;
-			this.logListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.logListBox_MouseDoubleClick);
-			// 
-			// SourceBox
-			// 
-			this.SourceBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.SourceBox.HideSelection = false;
-			this.SourceBox.Location = new System.Drawing.Point(12, 27);
-			this.SourceBox.Multiline = true;
-			this.SourceBox.Name = "SourceBox";
-			this.SourceBox.Size = new System.Drawing.Size(778, 454);
-			this.SourceBox.TabIndex = 1;
-			this.SourceBox.WordWrap = false;
-			// 
-			// saveFileDialog
-			// 
-			this.saveFileDialog.Filter = "PySharp Files (*.ps) |*.ps";
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(802, 553);
-			this.Controls.Add(this.SourceBox);
-			this.Controls.Add(this.logListBox);
-			this.Controls.Add(this.menuStrip1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.MainMenuStrip = this.menuStrip1;
-			this.Margin = new System.Windows.Forms.Padding(2);
-			this.Name = "Form1";
-			this.Text = "Compiler";
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(177, 24);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(177, 24);
+            this.saveToolStripMenuItem.Text = "Save as";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // buildToolStripMenuItem
+            // 
+            this.buildToolStripMenuItem.Name = "buildToolStripMenuItem";
+            this.buildToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.buildToolStripMenuItem.Text = "Build";
+            this.buildToolStripMenuItem.Click += new System.EventHandler(this.buildToolStripMenuItem_Click);
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.DefaultExt = "ps";
+            this.openFileDlg.Filter = "PySharp Files (*.ps) |*.ps";
+            // 
+            // logListBox
+            // 
+            this.logListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.logListBox.FormattingEnabled = true;
+            this.logListBox.ItemHeight = 16;
+            this.logListBox.Location = new System.Drawing.Point(0, 501);
+            this.logListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.logListBox.Name = "logListBox";
+            this.logListBox.Size = new System.Drawing.Size(1069, 180);
+            this.logListBox.TabIndex = 2;
+            this.logListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.logListBox_MouseDoubleClick);
+            // 
+            // SourceBox
+            // 
+            this.SourceBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SourceBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SourceBox.HideSelection = false;
+            this.SourceBox.Location = new System.Drawing.Point(0, 28);
+            this.SourceBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SourceBox.Multiline = true;
+            this.SourceBox.Name = "SourceBox";
+            this.SourceBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SourceBox.Size = new System.Drawing.Size(1069, 473);
+            this.SourceBox.TabIndex = 1;
+            this.SourceBox.WordWrap = false;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "PySharp Files (*.ps) |*.ps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 681);
+            this.Controls.Add(this.SourceBox);
+            this.Controls.Add(this.logListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Compiler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
