6ba7288d0a94a20eaa5bd19ae3f68af622de67fd
invisicat <eldest_dragon@mail.ru>
2014-01-09 21:45:27 +0400

    подсветка интов

--- src/compiler/utils/TokenColor.cs
+++ src/compiler/utils/TokenColor.cs
@@ -11,21 +11,22 @@ namespace compiler.utils
 	{
 		private static readonly Dictionary<TokenType, System.Drawing.Color> s_colors = new Dictionary<TokenType, System.Drawing.Color>
 		{
-			{TokenType.BOOL	   , Color.Blue},
-			{TokenType.CHAR	   , Color.Blue},
-			{TokenType.CLASS   , Color.Blue},
-			{TokenType.CONST   , Color.Blue},
-			{TokenType.ELSE	   , Color.Blue},
-			{TokenType.ENUM	   , Color.Blue},
-			{TokenType.FALSE   , Color.Blue},
-			{TokenType.IF	   , Color.Blue},
-			{TokenType.INT	   , Color.Blue},
-			{TokenType.PRIVATE , Color.Blue},
-			{TokenType.PUBLIC  , Color.Blue},
-			{TokenType.RETURN  , Color.Blue},
-			{TokenType.STATIC  , Color.Blue},
-			{TokenType.TRUE	   , Color.Blue},
-			{TokenType.WHILE   , Color.Blue},
+			{TokenType.BOOL				, Color.Blue},
+			{TokenType.CHAR				, Color.Blue},
+			{TokenType.CLASS			, Color.Blue},
+			{TokenType.CONST			, Color.Blue},
+			{TokenType.ELSE				, Color.Blue},
+			{TokenType.ENUM				, Color.Blue},
+			{TokenType.FALSE			, Color.Blue},
+			{TokenType.IF				, Color.Blue},
+			{TokenType.INT				, Color.Blue},
+			{TokenType.PRIVATE			, Color.Blue},
+			{TokenType.PUBLIC			, Color.Blue},
+			{TokenType.RETURN			, Color.Blue},
+			{TokenType.STATIC			, Color.Blue},
+			{TokenType.TRUE				, Color.Blue},
+			{TokenType.WHILE			, Color.Blue},
+			{TokenType.INTEGER_VALUE	, Color.Green}, // TODO: CHAR_VALUE, FLOAT_VALUE...
 		};
 
 		public static Color GetColor(Token token)
